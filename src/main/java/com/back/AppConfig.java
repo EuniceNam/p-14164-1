c6e51f1efe54830a8e922d6c2e62dad8ca819254
EuniceNam <caellime@gmail.com>
2026-05-11 14:40:55 +0900

    add: ApplicationRunner

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +9,21 @@ public class AppConfig {
     @Bean
     int version() {
         return 55;
+    }
+
+    @Bean
+    ApplicationRunner myApplicationRunner() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1");
+    }
+
+    private void work2() {
+        System.out.println("work2");
     }
 }
