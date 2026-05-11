a37d9f1e9ae2c4e4773415697ecb4430ace225f0
EuniceNam <caellime@gmail.com>
2026-05-11 12:41:50 +0900

    refactor

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -5,9 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    PersonService personService() {
-        System.out.println("postService 빈 생성됨!");
-        return new PersonService();
-    }
+
 }
