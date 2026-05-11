fdbd480676a868260c44933cd4b5af194fd00b9e
EuniceNam <caellime@gmail.com>
2026-05-11 12:49:37 +0900

    add: 이름에 따른 빈 선택

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -5,5 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    PersonRepository personRepository() {
+        return new PersonRepository(1);
+    }
 
+    @Bean
+    PersonRepository personRepositoryV2() {
+        return new PersonRepository(2);
+    }
 }
